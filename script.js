9413807200fed941156455bc167cc3368391eb2f
Naman Khattar <khattarnaman915@gmail.com>
2024-05-21 10:31:26 +0530

    Menu changes

---
+++ script.js
@@ -0,0 +1,8 @@
+function hamburgerMenu(){
+    let menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
